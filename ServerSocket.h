9e4f3d6bfc57d3f9e97b2104d764cb25555e1293
westmood1219 <156566837+westmood1219@users.noreply.github.com>
2026-02-20 10:29:44 +0800

    完成鼠标消息的处理

--- ServerSocket.h
+++ ServerSocket.h
@@ -112,6 +112,20 @@ public:
     std::string strOut;//整个包的数据
 };
 #pragma pack(pop)
+
+typedef struct MouseEvent
+{
+    MouseEvent() {
+        nAction = 0;
+        nButton = -1;
+        ptXY.x = 0;
+        ptXY.y = 0;
+    }
+    WORD nAction;// 点击, 移动, 双击
+    WORD nButton;// 左键, 右键, 中键
+    POINT ptXY;// 坐标
+}MOUSEEV, *PMOUSEEV;
+
 #pragma warning(push)
 #pragma warning(disable: 4267)// 暂时禁用 size_t 转 DWORD 的警告
 class CServerSocket
@@ -182,6 +196,14 @@ public:
     bool GetFilePath(std::string& strPath) {
         if ((m_packet.sCmd >= 2) && (m_packet.sCmd <= 4)) {
             strPath = m_packet.strData;
+            return true;
+        }
+        return false;
+    }
+
+    bool GetMouseEvent(const MOUSEEV& mouse) {
+        if (m_packet.sCmd == 5) {
+            memcpy((void*) & mouse, m_packet.strData.c_str(), sizeof(MOUSEEV));
             return true;
         }
         return false;
